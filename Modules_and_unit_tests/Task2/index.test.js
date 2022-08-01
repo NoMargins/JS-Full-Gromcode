e203d6b72015ead615f840861a3f481dccbfa00b
NoMargins <olenaryabenko@gmail.com>
2022-08-01 13:06:50 +0300

    first commit

---
+++ Modules_and_unit_tests/Task2/index.test.js
@@ -0,0 +1,19 @@
+import importType from 'eslint-plugin-import/lib/core/importType';
+import { getSquaredArray, getOddNumbers } from './calculator.js';
+import getSum from './calculator.js';
+
+it('if 2*2 = 4', () => {
+	const result = getSquaredArray([1, 3, 2]);
+	expect(result).toEqual([1, 9, 4]);
+});
+
+it('is there odd numbers', () => {
+	const result = getOddNumbers([1, 2, 3, 4, 5]);
+	expect(result).toEqual([1, 3, 5]);
+});
+
+it('is 2+3=5', () => {
+	const a = 2;
+	const b = 3;
+	expect(getSum).toEqual(5);
+});
