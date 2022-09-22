e5a6a6dc764abc493511efe07920aa171768dc6f
NoMargins <olenaryabenko@gmail.com>
2022-09-22 18:29:31 +0300

    prev upgrades deleted

--- LocalStorage/Task3/src/scripts/doneStatusChecker.js
+++ LocalStorage/Task3/src/scripts/doneStatusChecker.js
@@ -3,36 +3,34 @@ import { renderTasks } from './renderTasks.js';
 
 const onChange = (event) => {
 	event.stopPropagation();
-	const isCheckbox = event.target.classList.contains('.list__item-checkbox');
 	const storage = getItem('tasksList');
 
-	if (!isCheckbox) {
-		return;
-	} else {
-		[...document.querySelectorAll('.list__item-checkbox')].map((el) => {
-			if (
-				el.getAttribute('id') === event.target.getAttribute('id') &&
-				el.checked
-			) {
-				storage
-					.filter((task) => task.id === event.target.getAttribute('id'))
-					.map((task) => (task.done = true));
-			}
-			if (
-				el.getAttribute('id') === event.target.getAttribute('id') &&
-				!el.checked
-			) {
-				storage
-					.filter((task) => task.id === event.target.getAttribute('id'))
-					.map((task) => (task.done = false));
-			}
-			return storage;
-		});
-		setItem('tasksList', storage);
-		renderTasks();
-	}
+	[...document.querySelectorAll('.list__item-checkbox')].map((el) => {
+		if (
+			el.getAttribute('id') === event.target.getAttribute('id') &&
+			el.checked
+		) {
+			storage
+				.filter((task) => task.id === event.target.getAttribute('id'))
+				.map((task) => (task.done = true));
+		}
+		if (
+			el.getAttribute('id') === event.target.getAttribute('id') &&
+			!el.checked
+		) {
+			storage
+				.filter((task) => task.id === event.target.getAttribute('id'))
+				.map((task) => (task.done = false));
+		}
+		return storage;
+	});
+	return setItem('tasksList', storage);
 };
 
 export const changeFunction = () => {
-	document.querySelector('.list').addEventListener('change', onChange);
+	document.querySelector('.list').addEventListener('change', function (event) {
+		event.stopPropagation();
+		onChange(event);
+		renderTasks();
+	});
 };
