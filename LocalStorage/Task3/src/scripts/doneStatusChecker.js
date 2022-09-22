490f9191777f5d1c0de8f71e751ed5a7df6658a4
NoMargins <olenaryabenko@gmail.com>
2022-09-22 18:24:44 +0300

    minor changes

--- LocalStorage/Task3/src/scripts/doneStatusChecker.js
+++ LocalStorage/Task3/src/scripts/doneStatusChecker.js
@@ -3,29 +3,34 @@ import { renderTasks } from './renderTasks.js';
 
 const onChange = (event) => {
 	event.stopPropagation();
-
+	const isCheckbox = event.target.classList.contains('.list__item-checkbox');
 	const storage = getItem('tasksList');
-	[...document.querySelectorAll('.list__item-checkbox')].map((el) => {
-		if (
-			el.getAttribute('id') === event.target.getAttribute('id') &&
-			el.checked
-		) {
-			storage
-				.filter((task) => task.id === event.target.getAttribute('id'))
-				.map((task) => (task.done = true));
-		}
-		if (
-			el.getAttribute('id') === event.target.getAttribute('id') &&
-			!el.checked
-		) {
-			storage
-				.filter((task) => task.id === event.target.getAttribute('id'))
-				.map((task) => (task.done = false));
-		}
-		return storage;
-	});
-	setItem('tasksList', storage);
-	renderTasks();
+
+	if (!isCheckbox) {
+		return;
+	} else {
+		[...document.querySelectorAll('.list__item-checkbox')].map((el) => {
+			if (
+				el.getAttribute('id') === event.target.getAttribute('id') &&
+				el.checked
+			) {
+				storage
+					.filter((task) => task.id === event.target.getAttribute('id'))
+					.map((task) => (task.done = true));
+			}
+			if (
+				el.getAttribute('id') === event.target.getAttribute('id') &&
+				!el.checked
+			) {
+				storage
+					.filter((task) => task.id === event.target.getAttribute('id'))
+					.map((task) => (task.done = false));
+			}
+			return storage;
+		});
+		setItem('tasksList', storage);
+		renderTasks();
+	}
 };
 
 export const changeFunction = () => {
