2dbe6e4d2f9623325502c75d52c42a289af86c17
NoMargins <olenaryabenko@gmail.com>
2022-10-07 19:55:44 +0300

    minor changes

--- Async_Await/Task2/scripts/index.js
+++ Async_Await/Task2/scripts/index.js
@@ -4,10 +4,10 @@ import { renderUserData } from './user.js';
 import { fetchUserData } from './gateway.js';
 
 const submitBtnElem = document.querySelector('.name-form__btn');
-const imgElem = document.querySelector('.user__avatar');
+// const imgElem = document.querySelector('.user__avatar');
 const userNameElem = document.querySelector('.name-form__input');
 // imgElem.src = 'https://avatars3.githubusercontent.com/u10001';
-imgElem.src = 'https://avatars.githubusercontent.com/u/895678?s=400&v=4';
+// imgElem.src = 'https://avatars.githubusercontent.com/u/895678?s=400&v=4';
 
 const defaultUser = {
 	name: '',
